39cffe3f1cd9f16a37b049dade7c9d7183c00198
weilin.cao <weilin.cao@langlive.com>
2020-09-01 18:14:40 +0800

    learnDemo

---
+++ struct/demo2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Grade string `json:"grade,omitempty"`
+}
+
+func main() {
+	stu1 := Student{
+		Name:  "Tom",
+		Age:   18,
+		Grade: "middle school",
+	}
+
+	stu2 := Student{
+		Name: "LiLy",
+		Age:  19,
+	}
+
+	stuByts1, _ := json.Marshal(&stu1)
+
+	stuByts2, _ := json.Marshal(&stu2)
+
+	fmt.Println("stu1:", string(stuByts1))
+	fmt.Println("stu2:", string(stuByts2))
+}
