fc02e19c7fc37163b3bbcd10a601b4eee3dbe48f
weilin.cao <weilin.cao@langlive.com>
2020-09-11 18:35:34 +0800

    learnDemo

---
+++ datatype/map1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	ages := make(map[string]int)
+	ages["alice"] = 31
+	ages["charlie"] = 34
+	for name, age := range ages {
+		fmt.Printf("%s\t%d\n", name, age)
+	}
+}
