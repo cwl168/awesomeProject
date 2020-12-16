ca4f3788a423c0f4e7a4a3fe19e6f0e4eb337378
weilin.cao <weilin.cao@langlive.com>
2020-12-16 12:02:16 +0800

    helloworld grpc

--- helloWorldGrpc/server/server.go
+++ helloWorldGrpc/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	pb "learnDemo/helloWorldGrpc/proto"
 	"log"
 	"net"
@@ -27,9 +28,15 @@ func main() {
 	// 实例化grpc server
 	s := grpc.NewServer()
 
+	reflection.Register(s)
 	// 注册server服务
 	pb.RegisterGreeterServer(s, &server{})
 
 	log.Println("Listen on 127.0.0.1:8888...")
 	s.Serve(listen)
 }
+
+// 查询服务列表 grpcurl -plaintext  localhost:8888 list
+// 查询服务提供的方法 grpcurl -plaintext  localhost:8888 list helloworld.Greeter
+// 查看更详细的描述  grpcurl -plaintext  localhost:8888 describe helloworld.Greeter
+// 调用服务的方法  grpcurl -plaintext  localhost:8888 helloworld.Greeter.SayHello
