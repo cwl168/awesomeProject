ce0f95a75b1707556ddd66bcd2546de87ada25a4
caoweilin <7853376+caoweilin@user.noreply.gitee.com>
2021-03-22 22:59:35 +0800

    learnDemo

--- mergeChannel/mergeChannel1.go
+++ mergeChannel/mergeChannel1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -13,7 +14,7 @@ func main() {
 	b := asChan(10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
 	c := asChan(20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
 	//当channel被发送数据的协程关闭时，range就会结束，接着退出for循环
-	for v := range merge4(a, b, c) {
+	for v := range mergeReflect(a, b, c) {
 		fmt.Println(v)
 	}
 }
@@ -69,7 +70,8 @@ func merge3(cs ...<-chan int) <-chan int {
 	return out
 }
 
-func merge4(cs ...<-chan int) <-chan int {
+//way one
+func merge(cs ...<-chan int) <-chan int {
 	out := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(len(cs))
@@ -86,6 +88,74 @@ func merge4(cs ...<-chan int) <-chan int {
 		close(out)
 	}()
 	return out
+}
+
+//way two
+func mergeReflect(chans ...<-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		var cases []reflect.SelectCase
+		for _, c := range chans {
+			cases = append(cases, reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(c),
+			})
+		}
+
+		for len(cases) > 0 {
+			i, v, ok := reflect.Select(cases)
+			if !ok {
+				cases = append(cases[:i], cases[i+1:]...)
+				continue
+			}
+			out <- v.Interface().(int)
+		}
+	}()
+	return out
+
+}
+
+// way three
+func mergeRec(chans ...<-chan int) <-chan int {
+	switch len(chans) {
+	case 0:
+		c := make(chan int)
+		close(c)
+		return c
+	case 1:
+		return chans[0]
+	default:
+		m := len(chans) / 2
+		return mergeTwo(
+			mergeRec(chans[:m]...),
+			mergeRec(chans[m:]...))
+	}
+}
+
+func mergeTwo(a, b <-chan int) <-chan int {
+	c := make(chan int)
+
+	go func() {
+		defer close(c)
+		for a != nil || b != nil {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
 }
 
 func asChan(vs ...int) <-chan int {
