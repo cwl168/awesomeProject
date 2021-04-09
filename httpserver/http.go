ba668dc4cb1e5c956cc93d054be835537fc84c33
weilin.cao <weilin.cao@langlive.com>
2021-04-09 19:07:57 +0800

    learnDemo

---
+++ httpserver/http.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+const goroutinueNum = 2000
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < goroutinueNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				wg.Done()
+			}()
+			//resp, err := http.NewRequest("GET","http://127.0.0.1:8000/voiceapp/host/config?ip=2",nil)
+			//resp, err := http.Get("http://127.0.0.1:8000/voiceapp/host/config?ip=2")
+			resp, err := http.Get("http://127.0.0.1:8000/voiceapp/host/config?ip=2")
+			if err != nil {
+				fmt.Println("err:", err)
+				return
+			}
+			defer resp.Body.Close()
+			//body, _ := ioutil.ReadAll(resp.Body)
+			//fmt.Println("resp:", string(body))
+		}()
+		//time.Sleep(time.Millisecond*10)
+	}
+	wg.Wait()
+	fmt.Println("done")
+}
